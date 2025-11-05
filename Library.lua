fcd6e75f86fa5de68cba592d1168495ae4953f93
josh <yudickinson67@gmail.com>
2025-11-04 21:52:37 -0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3237,7 +3237,7 @@ function Library:CreateWindow(...)
 
             local Container = Library:Create('Frame', {
                 BackgroundTransparency = 1;
-                Position = UDim2.new(0, 8, 0, 28);
+                Position = UDim2.new(0, 8, 0, 40);
                 Size = UDim2.new(1, -4, 1, -20);
                 ZIndex = 1;
                 Parent = BoxInner;
