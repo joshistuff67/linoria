c6f78f77398e6dd1c2b8eae57be10e25a9a9466f
josh <yudickinson67@gmail.com>
2025-11-05 15:29:24 -0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3227,7 +3227,7 @@ function Library:CreateWindow(...)
 
             local GroupboxLabel = Library:CreateLabel({
                 Size = UDim2.new(1, 0, 0, 18);
-                Position = UDim2.new(0, 4, 0, 4);
+                Position = UDim2.new(0, 4, 0, 2);
                 TextSize = 14;
                 Text = Info.Name;
                 TextXAlignment = Enum.TextXAlignment.Center;
@@ -3237,8 +3237,8 @@ function Library:CreateWindow(...)
 
             local Container = Library:Create('Frame', {
                 BackgroundTransparency = 1;
-                Position = UDim2.new(0, 2, 0, 20);
-                Size     = UDim2.new(1, -16, 1, -32);
+                Position = UDim2.new(0, 4, 0, 20);
+                Size = UDim2.new(1, -4, 1, -20);
                 ZIndex = 1;
                 Parent = BoxInner;
             });
